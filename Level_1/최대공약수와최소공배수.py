5c3308df290bea3dcc6943c68bec4e9a76322451
ketkat001 <ketkat001@gmail.com>
2020-03-07 15:13:19 +0900

    0307|일일알고리즘|최대공약수와최소공배수

--- Level_1/최대공약수와최소공배수.py
+++ Level_1/최대공약수와최소공배수.py
@@ -1,6 +1,19 @@
 def solution(n, m):
-    answer = []
+    answer =[]
+    if n >= m:
+        num1, num2 = n, m
+    else:
+        num1, num2 = m, n
+    while True:
+        if num1 % num2 == 0:
+            answer.append(num2)
+            break
+        else:
+            num1, num2 = num2, num1 % num2
+    answer.append(num2*(n//num2)*(m//num2))
     return answer
+
+
 
 
 print(solution(3, 12))

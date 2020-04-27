dc140abc6f1e0c7a8e2009cee3d94c5de923a90e
ketkat001 <ketkat001@gmail.com>
2020-04-27 12:38:55 +0900

    0427|일일알고리즘|H-index

---
+++ Level_2/H-index.py
@@ -0,0 +1,10 @@
+def solution(citations):
+    citations.sort()
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
+
+
+print(solution([5, 5, 5]))

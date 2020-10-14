c16d395a66776432f1e2ad7c12b17b0b0d1234d8
ketkat001 <ketkat001@gmail.com>
2020-10-14 09:22:15 +0900

    1014|일일알고리즘|2018카카오코딩테스트|후보키|2차실패(11번 테스트케이스)

--- Level_2/후보키.py
+++ Level_2/후보키.py
@@ -2,36 +2,41 @@ from itertools import combinations
 
 
 def solution(relation):
-    answer = []
-    temp = [[] for _ in range(len(relation[0]))]
-    for i in range(len(relation)):
-        for j in range(len(relation[0])):
-            temp[j].append(relation[i][j])
-    for i in range(1, len(relation)):
-        for comb in combinations(temp, i):
-            comb = list(comb)
-            temp_list = []
-            for l in range(len(comb[0])):
-                temp_word = ''
-                for com in comb:
-                    temp_word += com[l]
-                temp_list.append(temp_word)
-            if len(set(temp_list)) == len(relation):
-                answer.append(comb)
-    remove_list = []
-    for i in range(len(answer)-1):
-        for j in range(i+1, len(answer)):
-            count = 0
-            for p in range(len(answer[i])):
-                for q in range(len(answer[j])):
-                    if answer[i][p] == answer[j][q]:
-                        count += 1
-            if count == len(answer[i]):
-                remove_list.append(answer[j])
-    for rl in remove_list:
-        if rl in answer:
-            answer.remove(rl)
-    return len(answer)
+    candidate_key = []
+    columns = [[relation[i][j] for i in range(len(relation))] for j in range(len(relation[0]))]
+    print(columns)
+    for i in range(1, len(columns)+1):
+        for comb in combinations(columns, i):
+            key = list(comb)
+            validate_key = validate_candidate_key(key)
+            if validate_key:
+                minimality_key = check_minimality(key, candidate_key)
+                if minimality_key:
+                    candidate_key.append(key)
+    print(candidate_key)
+    answer = len(candidate_key)
+    return answer
 
-print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
-print(solution([['1', '1', '3'], ['3', '1', '2'], ['3', '2', '4']]))
+
+def validate_candidate_key(key):
+    validate_list = []
+    for i in range(len(key[0])):
+        word = ''
+        for j in range(len(key)):
+            word += key[j][i]
+        validate_list.append(word)
+    if len(set(validate_list)) == len(key[0]):
+        return True
+    return False
+
+
+def check_minimality(key, candidate_key):
+    for i in range(1, len(key)+1):
+        for comb in combinations(key, i):
+            if list(comb) in candidate_key:
+                return False
+    return True
+
+#print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
+print(solution([['1', '1', '3'], ['3', '1', '2'], ['3', '2', '3']]))
+print(solution([['a','aa'],['aa','a'],['a','a']]))

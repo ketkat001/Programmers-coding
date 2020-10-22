1cba26b1ff25faa147890878f87df04828f4e555
ketkat001 <ketkat001@gmail.com>
2020-10-22 19:32:22 +0900

    1022|일일알고리즘|2018카카오코딩테스트|방금그곡

--- Level_2/방금그곡.py
+++ Level_2/방금그곡.py
@@ -1,50 +1,44 @@
-from datetime import datetime
+def cal_time(start_time, end_time):
+    hour = int(end_time.split(':')[0]) - int(start_time.split(':')[0])
+    mt = int(end_time.split(':')[1]) - int(start_time.split(':')[1])
+    result = hour * 60 + mt
+    return [result]
+
+
+def change_melody(music):
+    music = music.replace('C#', 'c')
+    music = music.replace('D#', 'd')
+    music = music.replace('F#', 'f')
+    music = music.replace('G#', 'g')
+    music = music.replace('A#', 'a')
+    return music
 
 
 def solution(m, musicinfos):
-    answer = []
+    answer = '(None)'
     music_list = [[] for _ in range(len(musicinfos))]
-    temp = []
-    for i in range(len(m)):
-        if m[i] != '#':
-            temp.append(m[i])
-        else:
-            temp[-1] += '#'
-    m = temp
+
     for i in range(len(music_list)):
-
-
-
         music_list[i] = musicinfos[i].split(',')
-        music_list[i][3] = list(music_list[i][3])
-        temp = []
-        for j in range(len(music_list[i][3])):
-            if music_list[i][3][j] != '#':
-                temp.append(music_list[i][3][j])
-            else:
-                temp[-1] += '#'
-        music_list[i][3] = temp
+        music_list[i][:2] = cal_time(music_list[i][0], music_list[i][1])
+    for i in range(len(music_list)):
+        music_list[i][2] = change_melody(music_list[i][2])
 
     for i in range(len(music_list)):
-        start_time, end_time = music_list[i][0], music_list[i][1]
-        music_list[i][0] = str(datetime.strptime(end_time, '%H:%M') - datetime.strptime(start_time, '%H:%M'))[2:4]
-        music_list[i][0] = int(music_list[i][0])
-        music_length = len(music_list[i][3])
-        if music_list[i][0] > music_length:
-            music_list[i][3] = music_list[i][3] * (music_list[i][0] // music_length + 1)
-        music_list[i][3] = music_list[i][3][:music_list[i][0]]
-    for music in music_list:
-        target_length = len(m)
-        for i in range(len(music[3]) - target_length):
-            if m == music[3][i:i + target_length]:
-                if m[-1] != '#' and music[3][i + target_length] != '#':
-                    answer.append([music[0], music[2]])
-    if answer:
-        answer = sorted(answer, key=lambda x: x[0], reverse=True)[0][1]
-    else:
-        answer = '(None)'
+        music_length = music_list[i][0]
+        if music_length > len(music_list[i][2]):
+            music_list[i][2] = music_list[i][2] * (music_length // len(music_list[i][2]) + 1)
+        music_list[i][2] = music_list[i][2][:music_length]
 
+    target_music = change_melody(m)
+    result = []
+    for music in music_list:
+        if target_music in music[2]:
+            result.append([music[0], music[1]])
+    if result:
+        answer = sorted(result, key=lambda x:-x[0])[0][1]
     return answer
 
-print(solution('ABCDEFG', ['12:00,12:14,HELLO,CDEFGAB','13:00,13:05,WORLD,ABCDEF']))
-print(solution('ABC', ['12:00,12:14,HELLO,C#DEFGAB','13:00,13:05,WORLD,ABCDEF']))
+print(solution('ABCDEFG#', ['12:00,12:14,HELLO,CDEFGAB#','13:00,13:05,WORLD,ABCDE#F']))
+print(solution("ABC", ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
+print(solution("CC#BCC#BCC#BCC#B#", ["03:00,00:00,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"]))

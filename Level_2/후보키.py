860dd64c0be41a53e221e9c694f130c03c7f276b
ketkat001 <ketkat001@gmail.com>
2021-08-14 15:22:39 +0900

    0814|일일알고리즘|2019카카오블라인드공채|후보키|구현

--- Level_2/후보키.py
+++ Level_2/후보키.py
@@ -1,42 +1,41 @@
 from itertools import combinations
 
 
-def solution(relation):
-    candidate_key = []
-    columns = [[relation[i][j] for i in range(len(relation))] for j in range(len(relation[0]))]
-    print(columns)
-    for i in range(1, len(columns)+1):
-        for comb in combinations(columns, i):
-            key = list(comb)
-            validate_key = validate_candidate_key(key)
-            if validate_key:
-                minimality_key = check_minimality(key, candidate_key)
-                if minimality_key:
-                    candidate_key.append(key)
-    print(candidate_key)
-    answer = len(candidate_key)
-    return answer
+def check_minimality(comb_list, candidate_keys):
+    for i in range(1, len(comb_list)+1):
+        for comb in combinations(comb_list, i):
+            if list(comb) in candidate_keys:
+                return True
+    return False
 
 
-def validate_candidate_key(key):
-    validate_list = []
-    for i in range(len(key[0])):
-        word = ''
-        for j in range(len(key)):
-            word += key[j][i]
-        validate_list.append(word)
-    if len(set(validate_list)) == len(key[0]):
+def check_uniqueness(comb_list, columns):
+    validate_list = [tuple(columns[comb][i] for comb in comb_list) for i in range(len(columns[0]))]
+    if len(set(validate_list)) == len(columns[0]):
         return True
     return False
 
 
-def check_minimality(key, candidate_key):
-    for i in range(1, len(key)+1):
-        for comb in combinations(key, i):
-            if list(comb) in candidate_key:
-                return False
-    return True
+def solution(relation):
+    column, row = len(relation), len(relation[0])
+    candidate_keys = []
+    columns = {i: [] for i in range(row)}
+    for i in range(row):
+        temp = []
+        for j in range(column):
+            temp.append(relation[j][i])
+        columns[i] = temp
+    for p in range(1, column+1):
+        for comb in combinations(columns, p):
+            if check_minimality(comb, candidate_keys):
+                continue
+            if not check_uniqueness(comb, columns):
+                continue
+            candidate_keys.append(list(comb))
+    answer = len(candidate_keys)
+    return answer
 
-#print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
+print(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]]))
 print(solution([['1', '1', '3'], ['3', '1', '2'], ['3', '2', '3']]))
 print(solution([['a','aa'],['aa','a'],['a','a']]))
+print(solution([["100","100","ryan","music","2"], ["200","200","apeach","math","2"], ["300","300","tube","computer","3"], ["400","400","con","computer","4"], ["500","500","muzi","music","3"], ["600","600","apeach","music","2"]]))
